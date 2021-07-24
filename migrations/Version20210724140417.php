36565e0d871b42852b202adbc1dbfadc0979f344
Slimane <aitslim@gmail.com>
2021-07-24 16:05:21 +0200

    mep heroku x

--- migrations/Version20210724140417.php
+++ migrations/Version20210724140417.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20210724134514 extends AbstractMigration
+final class Version20210724140417 extends AbstractMigration
 {
     public function getDescription(): string
     {
